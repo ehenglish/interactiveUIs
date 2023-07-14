c9b0fc059665ecdd78d74118b79e697049620498
Emily <ehenglish04@gmail.com>
2023-07-14 10:16:39 -0400

    Initial Commit

---
+++ interactiveUIs/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  interactiveUIs
+//
+//  Created by Emily English on 7/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
+    
+    var body: some View {
+        VStack{
+            Text(textTitle)
+                .font(.title)
+            TextField("Type name here", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            Button("Submit name") {
+                print(name)
+                textTitle = "Welcome, \(name)!"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+        }.padding()
+
+        }
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
